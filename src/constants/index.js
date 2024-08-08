abb199f20ebd5b351b4b53b05b565924519cb32c
thanhle1547 <thanhle147.dev@gmail.com>
2024-08-08 11:17:23 +0700

    ✨ Add test route for checking firebase config files existed

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
 const httpCodes = require('./httpCodes');
+
+const firebaseConfigDir = '/etc/secrets';
 
 const constants = {};
 
@@ -10,5 +13,11 @@ constants.requestWhitelist = ['/favicon.ico', '/static', '/public', '/fav.png'];
 constants.httpCodes = httpCodes;
 
 constants.thirtyDaysInMints = 30 * 24 * 60;
+
+try {
+    constants.firebaseConfigCount = fs.readdirSync(firebaseConfigDir).length;
+} catch (error) {
+    constants.firebaseConfigCount = -1;
+}
 
 module.exports = constants;
--- src/routes/test.js
+++ src/routes/test.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const { firebaseConfigCount } = require('../constants');
 
-router.use('/', (req, res) => {
+router.all('/', (req, res) => {
   res.status(200).send({ status: 'ok', method: req.method });
+});
+
+router.get('/firebase-config-exist', (req, res) => {
+  res.status(200).send({
+    status: 'ok',
+    method: req.method,
+    result: firebaseConfigCount > 0,
+    meta: {
+      total: firebaseConfigCount,
+    },
+  });
 });
 
 module.exports = router;
